c75032d5f22e90ec183cddc388918d4a040fa6a1
jewelve <xiedajeeling@gmail.com>
2016-05-06 13:34:58 +0900

    delete 進捗 graph function

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
         width: 300,
-        height: 450,
+        height: 250,
     });
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
     mainWindow.on('closed', function() {
